35f70ab791a8a0d3845ba2e55e0b6974c42e708f
AgentAlpaca <153544073+AgentAlpaca@users.noreply.github.com>
2024-11-03 10:11:20 +0200

    Add files via upload

---
+++ Game.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Game : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+    }
+    private string word;
+    private string wrongGuess;
+    private string goodGuess;
+    char[] currentGuess;
+    public Game(string word, string wrongGuess, string goodGuess, char[] currentGuess)
+    {
+        this.word = word;
+        this.wrongGuess = wrongGuess;
+        this.goodGuess = goodGuess;
+        this.currentGuess = currentGuess;
+    }
+    public string getWord()
+    {
+        return this.word;
+    }
+    public void setWord(string word)
+    {
+
+        this.word = word;
+    }
+    public string getWrongGuess() { return this.wrongGuess; }
+    public void setWrongGuess(string wrongGuess) { this.wrongGuess = wrongGuess; }
+    public void setGoodGuess(string goodGuess) { this.goodGuess = goodGuess; }
+    public void setCurrentGuess(char[] CurrentGuess)
+    {
+        for (int i = 0; i < CurrentGuess.Length; i++)
+        {
+            this.currentGuess[i] = CurrentGuess[i];
+        }
+
+
+    }
+}
